4ade9c83e121ce1fca6efb54f83ae0b24c16a1c5
Arin Agrawal <79896874+arinagrawal05@users.noreply.github.com>
2025-11-09 12:58:22 +0530

    Update AWS provider and improve security settings

--- main.tf
+++ main.tf
@@ -4,29 +4,29 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.0.0"   # Old version. Security scanners often flag outdated providers.
+      version = "3.0.0"   # outdated
     }
   }
 }
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA1234567890VULNERABLE"   # Hardcoded secret for scanner detection
+  access_key = "AKIA1234567890VULNERABLE"
   secret_key = "super-insecure-hardcoded-secret"
 }
 
 resource "aws_s3_bucket" "public_bucket" {
   bucket = "vulnerable-test-bucket-example"
-  acl    = "public-read"    # Wide open permissions
+  acl    = "public-read"   # insecure
 
   versioning {
-    enabled = false         # Disabled versioning often flagged
+    enabled = false         # should be true
   }
 
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "none"   # Not valid and will be flagged
+        sse_algorithm = "AES256"   # valid (but we can mark encryption disabled separately)
       }
     }
   }
@@ -46,13 +46,13 @@ resource "aws_security_group" "bad_sg" {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]   # Fully open ingress
+    cidr_blocks = ["0.0.0.0/0"]   # insecure
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]   # Fully open egress
+    cidr_blocks = ["0.0.0.0/0"]   # insecure
   }
 }
